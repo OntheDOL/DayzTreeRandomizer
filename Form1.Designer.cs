b941618baa09d04878c5107c4b4d7796f00c9e4e
OntheDOL <OntheDOL@users.noreply.github.com>
2023-07-16 10:41:33 +0800

    initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,243 @@
+﻿
+namespace DayzTreeRandom
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(47, 31);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(34, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Scale";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(171, 72);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(15, 14);
+            this.checkBox1.TabIndex = 1;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(114, 28);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(62, 23);
+            this.textBox1.TabIndex = 2;
+            this.textBox1.Text = "1";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(47, 71);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(105, 15);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Random Rotation?";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(191, 28);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(62, 23);
+            this.textBox2.TabIndex = 4;
+            this.textBox2.Text = "1";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(132, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Min";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(207, 9);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(30, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Max";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(47, 170);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 7;
+            this.button1.Tag = "InputFileBtn";
+            this.button1.Text = "Select File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(47, 142);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(54, 15);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Input file";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(114, 134);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(531, 23);
+            this.textBox3.TabIndex = 9;
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(114, 215);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.ReadOnly = true;
+            this.textBox4.Size = new System.Drawing.Size(531, 23);
+            this.textBox4.TabIndex = 11;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(47, 223);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(62, 15);
+            this.label6.TabIndex = 10;
+            this.label6.Text = "Export File";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(301, 269);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 22);
+            this.button2.TabIndex = 12;
+            this.button2.Text = "Randomize";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(274, 31);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(287, 15);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "(Acceptable scales between 0.01 and 5 ( 5 is massive!)";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(207, 71);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(252, 15);
+            this.label8.TabIndex = 14;
+            this.label8.Text = "(Will choose random number between 1 - 360)";
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(86, 32);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(15, 14);
+            this.checkBox2.TabIndex = 15;
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(665, 325);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Dayz Tree Randomizer";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.CheckBox checkBox2;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+using System.Windows.Forms;
+
+namespace DayzTreeRandom
+{
+    public partial class Form1 : Form
+    {
+        private List<int[]> PositionInfo;
+        private double scaleMinInput;
+        private double scaleMaxInput;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {            
+            OpenFileDialog file = new OpenFileDialog();
+            file.Title = "Select File";
+            file.InitialDirectory = @"C:\";
+            file.Filter = "Terrain Builder Txt file only|*.txt";
+            file.FilterIndex = 1;
+            file.ShowDialog();
+            if (file.FileName != "")
+            {
+                textBox3.Text = file.FileName;               
+                textBox4.Text = Path.ChangeExtension(file.FileName, null) + "_resized.txt";
+            }
+            else
+            {
+                textBox3.Text = "";
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (!CheckInputData())
+            {                
+                return;
+            }
+            Random rnd = new Random();
+            var fileContent = InputFileContent(textBox3.Text);
+            var fileContentExported = new List<String>();
+            PositionInfo = new List<int[]>();
+            for (int i = 0; i < fileContent.Count; i++)
+            {
+                GetStringLocationInfo(fileContent[i]);
+            }
+            if (PositionInfo[0][4] == 0)
+            {
+                MessageBox.Show("Invalid data found in the txt file, Check file and try again.");
+                return;
+            }
+
+            string origLine;
+            string lineStart;            
+            string nextPart;            
+            int scaleMin = (int)(scaleMinInput * 1000);
+            int scaleMax = (int)(scaleMaxInput * 1000);
+
+            for (int i = 0; i < PositionInfo.Count; i++)
+            {                
+                origLine = fileContent[i];
+                StringBuilder newLine = new StringBuilder(origLine);
+                
+                if (checkBox1.Checked) // rotation is checked, create string up to existing rotation data and then randomize it
+                {
+                    lineStart = newLine.ToString(0, PositionInfo[i][0] + 1);
+                    newLine = new StringBuilder(lineStart);
+                    int rotation = rnd.Next(0, 360);
+                    newLine.Append(rotation);
+                }
+                else //if random rotate isn't ticked, start string after existing rotation data
+                {
+                    lineStart = newLine.ToString(0, PositionInfo[i][1]);
+                    newLine = new StringBuilder(lineStart);
+                }     
+                
+                if (checkBox2.Checked) // scaling is checked
+                {
+                    //append existing data after rotation up to the start of scale data
+                    nextPart = origLine.Substring(PositionInfo[i][1], PositionInfo[i][2] - PositionInfo[i][1] + 1);
+                    newLine.Append(nextPart);
+                    int scale = rnd.Next(scaleMin, scaleMax);
+                    float scaleDiv = (float)scale / 1000;
+                    newLine.Append(scaleDiv);
+                    nextPart = origLine.Substring(PositionInfo[i][3], fileContent[i].Length - PositionInfo[i][3]);
+                    newLine.Append(nextPart);
+                }
+                else // no scaling done, append all the remaining data
+                {
+                    nextPart = origLine.Substring(PositionInfo[i][1], fileContent[i].Length - PositionInfo[i][1]);
+                    newLine.Append(nextPart);
+                }                
+                
+                fileContentExported.Add(newLine.ToString());
+            }
+
+            StreamWriter sw = new StreamWriter(textBox4.Text);
+            foreach (var str in fileContentExported)
+            {
+                sw.WriteLine(str);
+            }
+            sw.Close();
+            MessageBox.Show("Export completed!");
+
+
+        }
+
+        /// <summary>
+        /// gets the location of the semi-colons in input file so we can preserve the data thats not being changed
+        /// </summary>
+        /// <param name="str"></param>
+        private void GetStringLocationInfo(string str)
+        {
+            int[] p = new int[5];
+            
+            char  t = ';';
+            int count = 0;
+            for (int i = 0; i < str.Length; i++)
+            {
+                if (str[i] == t)
+                {
+                    count++;
+                    if (count == 3)
+                    {
+                        p[0] = i;
+                    }
+                    else if(count == 4)
+                    {
+                        p[1] = i;
+                    }
+                    else if (count == 6)
+                    {
+                        p[2] = i;
+                    }
+                    else if (count == 7)
+                    {
+                        p[3] = i;
+                    }
+                    else if (count == 8)
+                    {
+                        p[4] = i;
+                    }
+                }                
+            }
+            PositionInfo.Add(p);            
+        }
+
+        /// <summary>
+        /// grabs input file data and puts it into a list for later use
+        /// </summary>
+        /// <param name="filePath"></param>
+        /// <returns></returns>
+        private static List<string> InputFileContent(string filePath)
+        {
+            List<string> fileContent = new List<string>();
+            StreamReader sr = new StreamReader(filePath);
+            while (!sr.EndOfStream)
+            {
+                fileContent.Add(sr.ReadLine());
+            };
+            sr.Close();
+            return fileContent;
+        }
+
+        /// <summary>
+        /// some basic data checks to stop some errors
+        /// </summary>
+        /// <returns></returns>
+        private bool CheckInputData()
+        {
+            // check scale data correct
+            if (checkBox2.Checked)
+            {
+                if (!double.TryParse(textBox1.Text, out double parsedValue1) || !double.TryParse(textBox2.Text, out double parsedValue2))
+                {
+                    MessageBox.Show("scale size is invalid");
+                    return false;
+                }
+                else if (parsedValue1 > parsedValue2)
+                {
+                    MessageBox.Show("scale size is invalid - Min is greater than Max!");
+                    return false;
+                }
+                else if (parsedValue1 == parsedValue2)
+                {
+                    MessageBox.Show("scale size is invalid - Min is equal to Max, uncheck scale if you dont want to scale");
+                    return false;
+                }
+                else if (parsedValue2 > 5)
+                {
+                    MessageBox.Show("scale size is invalid - Max value is greater than limit of 5");
+                    return false;
+                }
+                else
+                {
+                    scaleMinInput = Math.Round(parsedValue1, 2);
+                    scaleMaxInput = Math.Round(parsedValue2, 2);
+                }
+            }
+            if (string.IsNullOrEmpty(textBox3.Text))
+            {
+                MessageBox.Show("No input file has been selected!");
+                return false;
+            }
+            if (!checkBox1.Checked && !checkBox2.Checked)
+            {
+                MessageBox.Show("Neither scale or rotation is checked, need at least one checked to do some work!");
+                return false;
+            }
+            return true;
+        }         
+    }
+}
